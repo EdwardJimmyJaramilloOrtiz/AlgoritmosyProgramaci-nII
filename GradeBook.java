f2efd4c09d09f7a5099d6521c48955a3b766a864
EdwardJimmyJaramilloOrtiz <126105534+EdwardJimmyJaramilloOrtiz@users.noreply.github.com>
2023-02-22 20:29:03 -0500

    Add files via upload

--- GradeBook.java
+++ GradeBook.java
@@ -1,35 +1,13 @@
-// Fig. 3.10: GradeBook.java
-// GradeBook class with a constructor to initialize the course name.
+// Fig. 3.4: GradeBook.java
+// Class declaration with a method that has a parameter.
 
 public class GradeBook
 {
-   private String courseName; // course name for this GradeBook
-
-   // constructor initializes courseName with String supplied as argument
-   public GradeBook( String name )
-   {
-      courseName = name; // initializes courseName
-   } // end constructor
-
-   // method to set the course name
-   public void setCourseName( String name )
-   {
-      courseName = name; // store the course name
-   } // end method setCourseName
-
-   // method to retrieve the course name
-   public String getCourseName()
-   {
-      return courseName;
-   } // end method getCourseName
-
    // display a welcome message to the GradeBook user
-   public void displayMessage()
+   public void displayMessage( String courseName )
    {
-      // this statement calls getCourseName to get the 
-      // name of the course this GradeBook represents
       System.out.printf( "Welcome to the grade book for\n%s!\n", 
-         getCourseName() );
+         courseName );
    } // end method displayMessage
 
 } // end class GradeBook
--- GradeBookTest.java
+++ GradeBookTest.java
@@ -1,23 +1,27 @@
-// Fig. 3.11: GradeBookTest.java
-// GradeBook constructor used to specify the course name at the 
-// time each GradeBook object is created.
+// Fig. 3.5: GradeBookTest.java
+// Create GradeBook object and pass a String to 
+// its displayMessage method.
+import java.util.Scanner; // program uses Scanner
 
 public class GradeBookTest
 {
    // main method begins program execution
    public static void main( String args[] )
    { 
-      // create GradeBook object
-      GradeBook gradeBook1 = new GradeBook( 
-         "CS101 Introduction to Java Programming" ); 
-      GradeBook gradeBook2 = new GradeBook( 
-         "CS102 Data Structures in Java" );
+      // create Scanner to obtain input from command window
+      Scanner input = new Scanner( System.in );
 
-      // display initial value of courseName for each GradeBook
-      System.out.printf( "gradeBook1 course name is: %s\n",
-         gradeBook1.getCourseName() );
-      System.out.printf( "gradeBook2 course name is: %s\n",
-         gradeBook2.getCourseName() );
+      // create a GradeBook object and assign it to myGradeBook
+      GradeBook myGradeBook = new GradeBook(); 
+
+      // prompt for and input course name
+      System.out.println( "Please enter the course name:" );
+      String courseName = input.nextLine(); // read a line of text
+      System.out.println(); // outputs a blank line
+
+      // call myGradeBook's displayMessage method 
+      // and pass courseName as an argument
+      myGradeBook.displayMessage( courseName );
    } // end main
 
 } // end class GradeBookTest
