5940be408b0dc51bbf5f80eb2279ccce0f9bfb7d
EdwardJimmyJaramilloOrtiz <126105534+EdwardJimmyJaramilloOrtiz@users.noreply.github.com>
2023-03-01 18:07:53 -0500

    Add files via upload

--- GradeBook.java
+++ GradeBook.java
@@ -1,13 +1,30 @@
-// Fig. 3.4: GradeBook.java
-// Class declaration with a method that has a parameter.
+// Fig. 3.7: GradeBook.java
+// GradeBook class that contains a courseName instance variable 
+// and methods to set and get its value.
 
 public class GradeBook
 {
-   // display a welcome message to the GradeBook user
-   public void displayMessage( String courseName )
+   private String courseName; // course name for this GradeBook
+
+   // method to set the course name
+   public void setCourseName( String name )
    {
+      courseName = name; // store the course name
+   } // end method setCourseName
+
+   // method to retrieve the course name
+   public String getCourseName()
+   {
+      return courseName;
+   } // end method getCourseName
+
+   // display a welcome message to the GradeBook user
+   public void displayMessage()
+   {
+      // this statement calls getCourseName to get the 
+      // name of the course this GradeBook represents
       System.out.printf( "Welcome to the grade book for\n%s!\n", 
-         courseName );
+         getCourseName() );
    } // end method displayMessage
 
 } // end class GradeBook
